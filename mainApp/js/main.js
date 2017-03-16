c084111cb2e7e56edbdb26cf92d637531a122533
momin-ctg <mominriyadh@gmail.com>
2017-03-16 17:21:29 +0600

    first commit

---
+++ mainApp/js/main.js
@@ -0,0 +1,13 @@
+/**
+ * Created by mominriyadh on 2/11/17.
+ */
+// TODO: Full Screen Video Url https://codepen.io/dudleystorey/pen/knqyK
+// TODO: Full Screen Video Url http://videojs.com/
+// TODO: NEED FACTORY DOX SVG ICON
+// TODO: NEED WRISBANDS IMAGE
+// TODO: RESPONSIVE DESIGNLAB
+
+// For DropToggle
+$( function() {
+    $( "#accordion" ).accordion();
+} );
